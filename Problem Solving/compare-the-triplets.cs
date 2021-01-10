6ae5cec2cade757a8a5269b1a64252e1152d7d4f
William Keller <kellerwilliam2011@gmail.com>
2021-01-10 13:22:32 -0300

    Problem solving pratices

---
+++ Problem Solving/compare-the-triplets.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution 
+{
+    static List<int> compareTriplets(List<int> a, List<int> b) 
+    {
+        int ra = 0;
+        int rb = 0;
+        
+        for (var x = 0; x < a.Count(); x++ )
+        {
+            if (a[x] > b[x]) 
+                ra++;
+            else if (a[x] < b[x]) 
+                rb++;
+        }   
+        
+        return new List<int>() { ra, rb };
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
